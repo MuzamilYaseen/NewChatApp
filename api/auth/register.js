251d3e002b8bff82aa3f7b83f81d3f49dcf22dea
MuzamilYaseen <muzamilyaseen99@gmail.com>
2025-11-24 11:27:51 +0500

    register done

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,2 +1,25 @@
-// import connectDB from '../../dbConnect';
+import connectDB from "../_db";
+import User from "../Models/User";
+import jwt from "jsonwebtoken";
 
+export async function handler(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+
+  if (!name) return res.status(400).json({ error: "name required" });
+  if (!email) return res.status(400).json({ error: "email required" });
+  if (!password) return res.status(400).json({ error: "password required" });
+
+  await connectDB();
+
+  const { name, email, password } = req.body;
+  const exists = await User.findOne({ email });
+
+  if (exists) return res.status(409).json({ message: "User exists" });
+
+  const user = await User.create({ name, email, password });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({ user, token });
+
+}
